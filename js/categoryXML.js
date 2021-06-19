eb148fbd4da9d19881fed05e3ada4bb24b3e6a3d
Maria-Blanco <mblanco6@unica.edu.ni>
2021-06-18 21:29:42 -0600

    01.3

--- js/categoryXML.js
+++ js/categoryXML.js
@@ -1,21 +1,30 @@
 let btnCat = document.getElementById('catInf');
 
-btnCat.onclick = function (){
-    let xhRequest = new XMLHttpRequest();
-
-    xhRequest.onreadystatechange = function (){
-        if (this.readyState === 4 && this.status === 200) {
-            listarCategorias (this);
-        }
+window.onload = function (){
+        let xhRequest = new XMLHttpRequest();
+    
+        xhRequest.onreadystatechange = function (){
+            if (this.readyState === 4 && this.status === 200) {
+                listarCategorias (this);
+            }
+        };
+    
+        xhRequest.open('GET', 'http://127.0.0.1:5500/categories.xml', true);
+        xhRequest.send();
     };
-
-    xhRequest.open('GET', 'http://127.0.0.1:5500/categories.xml', true);
-    xhRequest.send();
-};
 
 function listarCategorias(xml){
     let cat = xml.responseXML;
     let category = cat.getElementsByTagName('categoria');
 
-    console.log(category);
+    let names = document.getElementById('listarCategorias');
+        for (let i=0; i<category.length; i++) {
+            let catli = document.createElement('li');
+            let catNombre = document.createElement('a');
+            catNombre.innerHTML = category[i].getElementsByTagName('nombre')[0].textContent;
+            $(catNombre).attr("href","#");
+
+            names.appendChild(catli);
+            catli.appendChild(catNombre);
+        }
 }
